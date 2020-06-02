466d40ffee2f5e39e3c51614c2041b4ff1fd6e00
Eduard <yamiyugi43@gmail.com>
2020-06-02 14:40:46 -0500

    area

---
+++ area.cpp
@@ -0,0 +1,17 @@
+  #include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include "structures.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int create(void) {
+    
+}
