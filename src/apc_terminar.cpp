b882be88d8720f10a3f118be2de71039537e70b0
thelement115 <30273619+thelement115@users.noreply.github.com>
2020-06-05 13:29:47 -0500

    agregado control de errores a apc_terminar

--- src/apc_terminar.cpp
+++ src/apc_terminar.cpp
@@ -7,6 +7,14 @@
 
 
 int APC_Terminar(const char *nombre) {
-  shm_unlink(nombre);
+  if(nombre == NULL){
+    errno = APC_ERROR_ARGUMENTO;
+    return APC_FAIL;
+  }
+  fd = shm_unlink(nombre);
+  if (fd < 0){
+    errno = APC_ERROR_NOEXISTE;
+    return APC_FAIL;
+  }
   return 0;
 }
